da5f318d6894db5bf5f57d976ce3b5016407a94e
Risan <borhansiddque19@gmail.com>
2025-03-12 03:39:05 +0600

    Update category button functionality and add video category loading in index.html and script.js - task not end

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,19 @@ function loadVideos() {
     .then((data) => displayVideos(data.videos));
 }
 
+const loadCategoriesVideos = (id) => {
+
+  const url = `
+    https://openapi.programming-hero.com/api/phero-tube/category/${id}
+  `;
+  console.log(url);
+
+  fetch(url)
+  .then(res => res.json())
+  .then(data => console.log(data))
+  
+}
+
 function displayCategories(categories) {
   // Get The Container
   const categoryContainer = document.getElementById("category-container");
@@ -24,7 +37,7 @@ function displayCategories(categories) {
     // Create Element
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML = `
-      <button class="btn bg-gray-200 hover:bg-red-500 hover:text-white">${cat.category}</button>
+      <button onclick="loadCategoriesVideos(${cat.category_id})" class="btn btn-sm bg-gray-200 hover:bg-red-500 hover:text-white">${cat.category}</button>
     `;
     // Append The Element
     categoryContainer.append(categoryDiv);
@@ -37,7 +50,7 @@ const displayVideos = (videos) => {
     console.log(video);
 
     const videoCard = document.createElement("div");
-    videoCard.classList.add("card");
+    videoCard.classList.add("cursor-pointer");
     videoCard.innerHTML = `
       <div class="relative">
         <img src="${video.thumbnail}" alt="" class="w-full h-60 rounded-xl object-cover">
@@ -66,4 +79,3 @@ const displayVideos = (videos) => {
 };
 
 loadCategories();
-loadVideos();
