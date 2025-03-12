08c7e1266e33c7bbcc158d2f8c4cdff2474b1eaa
Risan <borhansiddque19@gmail.com>
2025-03-13 02:34:41 +0600

    Enhance video details functionality and improve HTML structure for better readability

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
 function removeActiveClass() {
   const activeButtons = document.getElementsByClassName("active");
-  for(let btn of activeButtons) {
-    btn.classList.remove('active');
+  for (let btn of activeButtons) {
+    btn.classList.remove("active");
   }
-  
 }
 
 function loadCategories() {
@@ -20,10 +19,10 @@ function loadCategories() {
 function loadVideos() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
-    .then((data) =>  {
+    .then((data) => {
       removeActiveClass();
-      document.getElementById('btn-all').classList.add('active');
-      displayVideos(data.videos)
+      document.getElementById("btn-all").classList.add("active");
+      displayVideos(data.videos);
     });
 }
 
@@ -36,9 +35,46 @@ const loadCategoriesVideos = (id) => {
     .then((data) => {
       removeActiveClass();
       const clickedButton = document.getElementById(`btn-${id}`);
-      clickedButton.classList.add('active');
+      clickedButton.classList.add("active");
       displayVideos(data.category);
     });
+};
+
+const loadVideoDetails = (videoId) => {
+  const url = `
+    https://openapi.programming-hero.com/api/phero-tube/video/${videoId}
+  `;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      displayVideoDetails(data.video);
+    });
+};
+
+const displayVideoDetails = (video) => {
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details-container");
+  detailsContainer.innerHTML = `
+    <div class="card bg-base-100 image-full shadow-sm ">
+      <figure>
+        <img
+        src="${video.thumbnail}"
+        alt="Video Thumbnail" />
+      </figure>
+      
+      <div class="card-body">
+        <div class="">
+          <div class="">
+            <img src="${video.authors[0].profile_picture}" class="w-20 h-20 rounded-full object-cover" />
+          </div>
+          <h3 class="text-lg font-semibold">${video.authors[0].profile_name}</h3>
+        </div>
+        <h2 class="card-title text-3xl">${video.title}</h2>
+        <p>${video.description}</p>
+      </div>
+      
+    </div>
+  `;
 };
 
 function displayCategories(categories) {
@@ -76,7 +112,6 @@ const displayVideos = (videos) => {
 
   videos.forEach((video) => {
     const videoCard = document.createElement("div");
-    videoCard.classList.add("cursor-pointer");
     videoCard.innerHTML = `
       <div class="relative">
         <img src="${video.thumbnail}" alt="" class="w-full h-60 rounded-xl object-cover">
@@ -92,12 +127,14 @@ const displayVideos = (videos) => {
         <div class="space-y-1">
           <h3 class="text-lg font-bold">${video.title}</h3>
           <div class="flex items-center gap-2">
-            <p class=" text-[#17171770]">${video.authors[0].profile_name}</p>
-            <img src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" class="w-6 h-6">
+            <p class=" text-[#17171770]">${video.authors[0].profile_name}
+            </p>
+            <p>${video.authors[0].verified == true ? `<img src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" class="w-6 h-6">` : ``}</p>
           </div>
           <p class="text-sm text-[#17171770]">${video.others.views} Views</p>
         </div>
       </div>
+      <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block bg-gray-300 cursor-pointer">Show Details</button>
     `;
 
     videosContainer.append(videoCard);
@@ -105,3 +142,4 @@ const displayVideos = (videos) => {
 };
 
 loadCategories();
+loadVideos();
